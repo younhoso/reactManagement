867755d467ef08bc507fbdc709b97b17e4047877
younhoso <thdbsgh3443@naver.com>
2019-08-14 18:25:58 +0900

    190814 커밋

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const os = require("os");
@@ -8,25 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public/')));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connetion = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  post: conf.post,
+  database: conf.database
+});
+connetion.connect(); //연결한 데이터를 실행
+
 app.get('/api/customers', (req, res)=>{
-    res.send([
-          {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name': '길동이',
-            'birthday': '961202',
-            'gender': '남자',
-            'job': '비회원'
-          },
-          {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name': '홍길동',
-            'birthday': '960305',
-            'gender': '여자',
-            'job': '회원'
-          },
-    ])
+  connetion.query(
+    "SELECT * FROM CUSTOMER2",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 app.listen(port, () => console.log(`porpt ${port}`));
